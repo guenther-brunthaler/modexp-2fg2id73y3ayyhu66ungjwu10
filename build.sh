e348f3145c24d08ad1125ccf16076f5fc77691da
Guenther Brunthaler <gb@xrpi3.emgenxx69lwyn5ctlr4nl64ul.local>
2017-08-20 07:20:45 +0200

    Make reassembly scripte more resource-efficient
    Avoid unnecessary subshell and collection of arguments in memory.

--- build.sh
+++ build.sh
@@ -1,11 +1,8 @@
 #! /bin/sh
 exec source_level_linker/unit_linker -o master -M '' -L `
-	set lib \`
-		for l in .submods/*
-		do
-			printf '%s\n' "${l##*/}"
-		done
-	\`
-	IFS=:
-	printf '%s\n' "$*"
+	printf %s lib
+	for l in .submods/*
+	do
+		printf ':%s' "${l##*/}"
+	done
 ` *.order

934a42686d717bc318e6c6465d27f12c30ec077d
Guenther Brunthaler <gb@xrpi3.emgenxx69lwyn5ctlr4nl64ul.local>
2017-09-06 07:47:25 +0200

    Allow overriding the output directory
    Also create it if it does not exist yet.

--- build.sh
+++ build.sh
@@ -1,5 +1,16 @@
 #! /bin/sh
-exec source_level_linker/unit_linker -o master -M '' -L `
+outdir=master
+set -e
+trap '$? = 0 || echo "$0 failed!" >& 2' 0
+while getopts o: opt
+do
+	case $opt in
+		o) outdir=$OPTARG;;
+		*) false || exit
+	esac
+done
+mkdir -p -- "$outdir"
+exec source_level_linker/unit_linker -o "$outdir" -M '' -L `
 	printf %s lib
 	for l in .submods/*
 	do
